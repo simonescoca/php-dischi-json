eaabcad277c5880f94493d465bdc29f662ca2eca
simonescoca <simone.scoca@icloud.com>
2023-07-05 15:25:43 +0200

    fixed server's data got from database

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
-    header ("Content_Type: application/json"); // preparo il server a ricevere un json dal database
+    header ("Content-Type: application/json"); // preparo il server a ricevere un json dal database
     $json_string = file_get_contents ("./database/data.json"); // ricevo il json grezzo sotto forma di stringa
-    $json_array = json_decode ($json_string); // trasformo il json stringa in json array
-    // var_dump($json_array);
+    echo $json_string;
+    // json_decode ($json_string) ; modifica al json ; json_encode ($json_string) 
 ?>
