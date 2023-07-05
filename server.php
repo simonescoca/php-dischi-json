404d54af333989a9a19b4e685b212db505fb2312
simonescoca <simone.scoca@icloud.com>
2023-07-05 14:40:23 +0200

    added comments

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
-    $json_string = file_get_contents ("./database/data.json");
-    header ("Content_Type: application/json");
-    $json_array = json_decode ($json_string);
+    header ("Content_Type: application/json"); // preparo il server a ricevere un json dal database
+    $json_string = file_get_contents ("./database/data.json"); // ricevo il json grezzo sotto forma di stringa
+    $json_array = json_decode ($json_string); // trasformo il json stringa in json array
     // var_dump($json_array);
 ?>
